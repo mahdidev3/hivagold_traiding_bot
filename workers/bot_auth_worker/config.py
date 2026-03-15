3b0faa4fa9a8b1f774217a7dbe5c92564365b21b
mahdidev3 <mahdi.habibi0311384@gmail.com>
2026-03-15 20:24:28 +0330

    make workers to accepts the mode arg

--- workers/bot_auth_worker/config.py
+++ workers/bot_auth_worker/config.py
@@ -1,7 +1,6 @@
 import os
 from dataclasses import dataclass
 from urllib.parse import urljoin
-from dotenv import load_dotenv
 
 
 @dataclass
--- workers/bot_trading_worker/run.py
+++ workers/bot_trading_worker/run.py
@@ -1,15 +1,32 @@
+import uvicorn
+from config import get_config
 import os
 import sys
+import argparse
+from dotenv import load_dotenv
 
 os.environ["PYTHONDONTWRITEBYTECODE"] = "1"
 sys.dont_write_bytecode = True
 
-import uvicorn
-from config import get_config
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "mode",
+        choices=["local", "docker"],
+        help="Run mode: local or docker",
+    )
+    return parser.parse_args()
+
 
 config = get_config()
 
 if __name__ == "__main__":
+    args = parse_args()
+
+    if args.mode == "local":
+        load_dotenv()
+
     uvicorn.run(
         "app.main:app",
         host=config.TRADING_WORKER_HOST,
